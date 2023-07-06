ec18d4b8b766c1d258575b2594363bbaedfce1a5
andreBass1201 <andre300.av@gmail.com>
2023-07-06 08:40:09 -0500

    Feact update dep

---
+++ src/test/java/com/company/market/MarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
